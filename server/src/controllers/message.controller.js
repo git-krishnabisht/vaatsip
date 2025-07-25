074dba127149d8f1fea632f5bf9018db84bd42b1
krish <krishnaasinghbisht@gmail.com>
2025-07-25 20:36:32 +0530

    commit-54

--- server/src/controllers/message.controller.js
+++ server/src/controllers/message.controller.js
@@ -2,6 +2,14 @@ import db from "../config/db.config.js";
 import imageType from "image-type";
 import { getReceiverSocketId, io } from "../socket/socket.js";
 
+function encode64(image, type) {
+  if (!image || !type) return { success: false, data: undefined };
+  return {
+    success: true,
+    data: `data:${type};base64,${image.toString("base64")}`,
+  };
+}
+
 export const sendMessages = async (req, res) => {
   try {
     const {
@@ -12,45 +20,50 @@ export const sendMessages = async (req, res) => {
     } = req;
 
     if (!sender || !receiver) {
-      return res
-        .status(400)
-        .json({ error: "Sender and receiver are required" });
+      return res.status(400).json({
+        error: "You much provide both sender username and receiver username",
+      });
     }
     if (!message && !file) {
       return res
         .status(400)
-        .json({ error: "Either message or file must be provided" });
+        .json({ error: "You must provide a message or a valid image." });
     }
 
     await db.query("BEGIN");
-    const conversationQuery = await db.query(
-      "INSERT INTO conversation(sender, receiver, message) VALUES ($1, $2, $3) RETURNING *",
+    const messageQuery = await db.query(
+      "INSERT INTO messages(sender, receiver, message) VALUES ($1, $2, $3) RETURNING *",
       [sender, receiver, message || null]
     );
 
-    const { created_at, message_id: messageId } = conversationQuery.rows[0];
+    const { created_at, message_id: messageId } = messageQuery.rows[0];
     let image = null;
-    let imagetype = null;
+    let type = null;
     let base64Image = null;
 
     if (file) {
       image = file.buffer;
-      imagetype = file.mimetype;
+      type = file.mimetype;
       const attachmentQuery = await db.query(
         "INSERT INTO attachments(message_id, image_data, image_type) VALUES ($1, $2, $3) RETURNING *",
-        [messageId, image, imagetype]
+        [messageId, image, type]
       );
+
       image = attachmentQuery.rows[0].image_data;
-      const type = imageType(image)?.mime || "image/jpeg";
-      const temp = `data:${type};base64,${image.toString("base64")}`;
-      base64Image = temp;
+
+      let _encoding = encode64(image, type);
+      if (!_encoding.success || !_encoding.data) {
+        return res
+          .status(400)
+          .json({ error: "You must provide valid image data and type." });
+      }
+      base64Image = _encoding.data;
     }
 
     await db.query("COMMIT");
     const socketId = getReceiverSocketId(receiver);
-    console.log("socket id: ", socketId);
     if (socketId) {
-      const msg = conversationQuery.rows[0].message;
+      const msg = messageQuery.rows[0].message;
       io.to(socketId).emit("newMessage", {
         msg,
         base64Image,
@@ -89,16 +102,16 @@ export const getMessages = async (req, res) => {
     const query = await db.query(
       `
       SELECT 
-        c.message, 
+        m.message, 
         a.image_data, 
-        c.sender, 
-        c.receiver, 
-        c.created_at 
-      FROM conversation c 
-      LEFT JOIN attachments a ON c.message_id = a.message_id 
-      WHERE (c.sender = $1 AND c.receiver = $2) 
-         OR (c.sender = $2 AND c.receiver = $1) 
-      ORDER BY c.created_at
+        m.sender, 
+        m.receiver, 
+        m.created_at 
+      FROM messages m
+      LEFT JOIN attachments a ON m.message_id = a.message_id 
+      WHERE (m.sender = $1 AND m.receiver = $2) 
+         OR (m.sender = $2 AND m.receiver = $1) 
+      ORDER BY m.created_at
     `,
       [sender, receiver]
     );
@@ -117,9 +130,8 @@ export const getMessages = async (req, res) => {
       }
 
       const type = imageType(row.image_data)?.mime || "image/jpeg";
-      const base64Image = `data:${type};base64,${row.image_data.toString(
-        "base64"
-      )}`;
+      const base64Image = encode64(row.image_data, type);
+
       return {
         message: row.message,
         image_data: base64Image,
