2ca2a7eab36cb1f4d0eb1b1eb4c827d49f7d119e
krish <krishnaasinghbisht@gmail.com>
2025-08-14 00:32:07 +0530

    commit-63

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -1,61 +1,14 @@
-import { refreshGoogleToken } from "../oauth/oauth.refresh-google-token.js";
 import jwt from "jsonwebtoken";
-import db from "../config/db.config.js";
 
 export async function protectedRoute(req, res, next) {
-  const token = req.cookies.jwt;
-
-  if (!token) return res.sendStatus(401);
-
   try {
+    const token = req.cookies.jwt;
+
+    if (!token) return res.sendStatus(401);
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     return next();
   } catch (err) {
-    // JWT refreshing on expiry
-    if (err.name === "TokenExpiredError") {
-      const googleId = req.cookies.googleId;
-      if (!googleId) return res.sendStatus(401);
-
-      const result = await db.query(
-        "select * from users WHERE google_id = $1",
-        [googleId]
-      );
-
-      if (result.rows.length > 0) {
-        const refreshToken = result.rows[0].refresh_token;
-        if (refreshToken) {
-          const refreshedData = await refreshGoogleToken(refreshToken);
-
-          if (refreshedData.id_token) {
-            const payload = jwt.decode(refreshedData.id_token);
-            const newToken = jwt.sign({
-              googleId: payload.sub,
-              email: payload.email,
-              name: payload.name,
-            },
-              process.env.JWT_SECRET,
-              { expiresIn: "1h" }
-            );
-
-            res.cookie("jwt", newToken, {
-              httpOnly: true,
-              secure: false,
-              sameSite: "lax",
-              maxAge: 7 * 24 * 60 * 60 * 1000,
-            });
-
-            req.user = {
-              googleId: payload.sub,
-              email: payload.email,
-              name: payload.name,
-            };
-            return next();
-          }
-        }
-      }
-      return res.sendStatus(401);
-    }
-    return res.sendStatus(403);
+    return res.status(500).json({ error: "Internal server error" });
   }
 }
